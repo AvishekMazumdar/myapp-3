87520f762cc59b12e873a65577c86165ff9a0e93
Avishek <avi_mazumdar@yahoo.com>
2018-08-24 18:39:25 +0000

    New changes

---
+++ src/main/java/io/openshift/booster/service/Customendpoint.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016-2017 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.openshift.booster.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Component
+public class Customendpoint {
+    
+    @GET
+    @Produces("application/json")
+    @RequestMapping("/fetchAddressData")
+    public String fetchDogs() {
+        RestTemplate restTemplate = new RestTemplate();
+        String result = 
+            restTemplate.getForObject("https://api-adresse.data.gouv.fr/search/?q=Barcar", String.class);
+        return result;
+    }
+}
